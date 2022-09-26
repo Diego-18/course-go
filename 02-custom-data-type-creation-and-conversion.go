48d36ffa2fbcdd5d9b199428ce5c4b165b061dc6
Diego Chavez <ingdiegochavez18@gmail.com>
2022-09-25 20:02:01 -0500

    Update 02-custom-data-type-creation-and-conversion.go

--- 02-custom-data-type-creation-and-conversion.go
+++ 02-custom-data-type-creation-and-conversion.go
@@ -10,16 +10,15 @@ var phoneClient phone
 
 func main() {
 	a = 45
-	fmt.Println(a)
-	fmt.Printf("%T\n", a)
+	fmt.Println(a)		//45
+	fmt.Printf("%T\n", a)	//int
 
 	phoneClient = 3015977425
-	fmt.Println(phoneClient)
-	fmt.Printf("%T\n", phoneClient)
+	fmt.Println(phoneClient)	//3015977425
+	fmt.Printf("%T\n", phoneClient)	//main.phone
 
 	// Ejemplo de conversion de tipo phone a int
 	a = int(phoneClient)
-	fmt.Println(a)
-	fmt.Printf("%T\n", a)
-
+	fmt.Println(a)			//3015977425
+	fmt.Printf("%T\n", a)		//int
 }
