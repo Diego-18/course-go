54218b2c89f414f575fe48f597c0e9278bad4c45
Diego Chavez <ingdiegochavez18@gmail.com>
2022-09-27 11:15:12 -0500

    Update 03-ej1-variables.go

--- 03-ej1-variables.go
+++ 03-ej1-variables.go
@@ -1,14 +1,15 @@
 /*
-Usa var para DECLARAR tres VARIABLES. 
-Las variables deben tener scope a nivel de paquete. 
-No asignar VALORES a las variables. 
-Usa los siguientes IDENTIFICADORES para las variables y asegúrate de que las variables son de los siguientes TIPOS (lo quiere decir que pueden almacenar VALORES de ese TIPO).
+Usando el operador de declaración corta, ASIGNA los siguientes VALORES a VARIABLES con los IDENTIFICADORES “x”, “y” y “z”
 
-identificador “x” tipo int
-identificador “y” tipo string
-identificador “z” tipo bool
+42
+“James Bond”
+true
 
-En main imprime los valores de cada identificador
+Luego imprime los valores almacenados en esas variables usando: 
+
+a) Una sola declaración de la función println
+b) Múltiples declaraciones de println
+
 */
 
 package main
@@ -20,9 +21,9 @@ func main() {
 	y := "James Bond"
 	z := true
 
-	fmt.Println(x, y, z)
+	fmt.Println(x, y, z)		//a
 
-	fmt.Println("El valor de x es: ", x)
-	fmt.Println("El valor de y es: ", y)
-	fmt.Println("El valor de z es: ", z)
+	fmt.Println("El valor de x es: ", x)	//b
+	fmt.Println("El valor de y es: ", y)	//b
+	fmt.Println("El valor de z es: ", z)	//b
 }
