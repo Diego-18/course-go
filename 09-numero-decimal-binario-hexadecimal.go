73e4b86b771427818dc2093a139ffa54515a84fb
Diego Chavez <ingdiegochavez18@gmail.com>
2022-09-28 10:02:30 -0500

    Create 09-numero-decimal-binario-hexadecimal.go

---
+++ 09-numero-decimal-binario-hexadecimal.go
@@ -0,0 +1,10 @@
+// Escribe un programa que imprima un número en decimal, binario, y hexadecimal
+
+package main
+
+import "fmt"
+
+func main() {
+	number := 42
+	fmt.Printf("%d\t%b\t%x", number, number, number)
+}
