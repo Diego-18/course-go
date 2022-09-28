1e4d274b9b87285284da1f3af3283db879d2e12d
Diego Chavez <ingdiegochavez18@gmail.com>
2022-09-28 11:29:21 -0500

    Create 15-for-init-condition-post.go

---
+++ 15-for-init-condition-post.go
@@ -0,0 +1,14 @@
+/*
+tabla del 5 usando el for con el formato estandar init; condition; post;
+*/
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 1; i <= 10; i++ {
+		fmt.Println(5 * i)
+	}
+
+}
