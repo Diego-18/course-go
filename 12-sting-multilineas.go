4e73869fbe6325d14b575b22e6f40257323659cd
Diego Chavez <ingdiegochavez18@gmail.com>
2022-09-28 10:35:23 -0500

    Create 12-sting-multilineas.go

---
+++ 12-sting-multilineas.go
@@ -0,0 +1,14 @@
+/*
+Crea una variable de tipo string usando un string literal no interpretado (raw string literal).
+Imprímelo.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	learning := `En estos momentos me encuentro aprendiendo GO
+	Buen lenguaje`
+	fmt.Println(learning)
+}
