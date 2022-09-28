12ca06ceb1b42560b4235deb934e64a96abd26c3
Diego Chavez <106765365+dchavez-vinix@users.noreply.github.com>
2022-09-28 17:19:17 -0500

    feat: add-slicen

---
+++ 07-Arrays y Slicen/02-Slicen.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var languages []string
+
+	// add values to slicen
+	languages = append(languages, "Go", "php", "Javascript", "Java")
+	fmt.Println(languages)
+}
